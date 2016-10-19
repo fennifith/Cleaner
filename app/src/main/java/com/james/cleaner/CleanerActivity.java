a8634f6d88485fc9251b5d63fac398b1d872a1b8
James Fenn <18jafenn90@gmail.com>
2016-10-19 06:27:36 -0400

    Simplified "cleaning" process

--- app/src/main/java/com/james/cleaner/CleanerActivity.java
+++ app/src/main/java/com/james/cleaner/CleanerActivity.java
@@ -6,17 +6,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class CleanerActivity extends AppCompatActivity {
@@ -63,28 +59,21 @@ public class CleanerActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast toast = new Toast(CleanerActivity.this);
-
-                        View snackbar = LayoutInflater.from(CleanerActivity.this).inflate(R.layout.snackbar_layout, null);
-                        ViewCompat.setElevation(snackbar, TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources().getDisplayMetrics()));
-
-                        toast.setView(snackbar);
-                        toast.setDuration(Toast.LENGTH_LONG);
-                        toast.setMargin(0, 0);
-                        toast.setGravity(Gravity.FILL_HORIZONTAL | Gravity.BOTTOM, 0, 0);
-                        toast.show();
-                    }
-                }, 1000);
-
                 try {
-                    Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE);
                     intent.setData(Uri.parse("package:" + getPackageName()));
                     startActivity(intent);
                 } catch ( ActivityNotFoundException e ) {
-                    startActivity(new Intent(android.provider.Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS));
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(CleanerActivity.this, R.string.action_uninstall, Toast.LENGTH_LONG).show();
+                        }
+                    }, 1000);
+
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS);
+                    intent.setData(Uri.parse("package:" + getPackageName()));
+                    startActivity(intent);
                 }
             }
         });
