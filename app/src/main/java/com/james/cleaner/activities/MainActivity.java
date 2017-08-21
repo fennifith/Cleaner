a96f7507620bfee84f36c03acc67da9f17353781
James Fenn <18jafenn90@gmail.com>
2017-08-20 21:11:13 -0400

    massive ui changes

--- app/src/main/java/com/james/cleaner/activities/MainActivity.java
+++ app/src/main/java/com/james/cleaner/activities/MainActivity.java
@@ -1,26 +1,60 @@
 package com.james.cleaner.activities;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Toast;
 
 import com.james.cleaner.R;
+import com.james.cleaner.views.AppIconView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppIconView imageView;
+    private AppCompatButton button;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
+        imageView = findViewById(R.id.icon);
+
+        button = findViewById(R.id.clean);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this, CleanerActivity.class));
-                finish();
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE);
+                    intent.setData(Uri.parse("package:" + getPackageName()));
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, R.string.action_uninstall, Toast.LENGTH_LONG).show();
+                        }
+                    }, 1000);
+
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS);
+                    intent.setData(Uri.parse("package:" + getPackageName()));
+                    startActivity(intent);
+                }
             }
-        }, 1500);
+        });
+
+        findViewById(R.id.info).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://txt.fyi/+/6599fc72/")));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/james/cleaner/views/AppIconView.java
@@ -0,0 +1,116 @@
+package com.james.cleaner.views;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.media.ThumbnailUtils;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
+
+import com.james.cleaner.R;
+import com.james.cleaner.utils.ImageUtils;
+
+public class AppIconView extends View {
+
+    private Bitmap fgBitmap;
+    private Paint paint;
+    private int size;
+    private float rotation = 90;
+    private float fgScale = 0, bgScale = 0;
+
+    private Path path;
+
+    public AppIconView(Context context) {
+        this(context, null);
+    }
+
+    public AppIconView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AppIconView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimaryDark));
+
+        ValueAnimator animator = ValueAnimator.ofFloat(bgScale, 0.8f);
+        animator.setInterpolator(new OvershootInterpolator());
+        animator.setDuration(2000);
+        animator.setStartDelay(500);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                bgScale = (float) animator.getAnimatedValue();
+                invalidate();
+            }
+        });
+        animator.start();
+
+        animator = ValueAnimator.ofFloat(rotation, 360);
+        animator.setInterpolator(new DecelerateInterpolator());
+        animator.setDuration(2000);
+        animator.setStartDelay(500);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                fgScale = animator.getAnimatedFraction() * 0.8f;
+                rotation = (float) animator.getAnimatedValue();
+                invalidate();
+            }
+        });
+        animator.start();
+    }
+
+    private Bitmap getRoundBitmap(@DrawableRes int drawable, int size) {
+        Bitmap bitmap = ImageUtils.drawableToBitmap(ContextCompat.getDrawable(getContext(), drawable));
+        bitmap = Bitmap.createBitmap(bitmap, bitmap.getWidth() / 6, bitmap.getHeight() / 6, (int) (0.666 * bitmap.getWidth()), (int) (0.666 * bitmap.getHeight()));
+        bitmap = ThumbnailUtils.extractThumbnail(bitmap, size, size);
+
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+
+        roundedBitmapDrawable.setCornerRadius(size / 2);
+        roundedBitmapDrawable.setAntiAlias(true);
+
+        return ImageUtils.drawableToBitmap(roundedBitmapDrawable);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int size = Math.min(canvas.getWidth(), canvas.getHeight());
+        if (this.size != size || fgBitmap == null || path == null) {
+            this.size = size;
+            fgBitmap = getRoundBitmap(R.mipmap.ic_launcher_foreground, size);
+            path = new Path();
+            path.arcTo(new RectF(0, 0, size, size), 0, 359);
+            path.close();
+        }
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(bgScale, bgScale, size / 2, size / 2);
+
+        Path path = new Path();
+        this.path.transform(matrix, path);
+        canvas.drawPath(path, paint);
+
+        matrix = new Matrix();
+        matrix.postTranslate(-fgBitmap.getWidth() / 2, -fgBitmap.getHeight() / 2);
+        matrix.postScale(fgScale, fgScale);
+        matrix.postRotate(rotation);
+        matrix.postTranslate(fgBitmap.getWidth() / 2, fgBitmap.getHeight() / 2);
+        canvas.drawBitmap(fgBitmap, matrix, paint);
+    }
+
+}
